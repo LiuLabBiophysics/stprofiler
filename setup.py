f6bec3c4030d0b800bd45539e079981c3008a42f
LiuLabIUPUI <liulabiupui@gmail.com>
2024-06-18 16:28:32 -0400

    fix(shorten_setup): shorten setup.py for poetry

--- setup.py
+++ setup.py
@@ -1,32 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-__version__ = '0.0.1'
-
-setup(
-    
-    name='STProfiler',
-    version=__version__,
-    long_description='Spatial Transcriptomics Profiler (STProfiler) is a python package for researchers to interpret spatial transcriptomics in single-cell level, \
-                      by extracting spatial transcriptomic features from smFISH images. \
-                      Package includes image analysis pipeline, spatial transcriptomic profile extraction pipeline and machine learning pipeline.',
-
-    author='Garrick Chang',
-    author_email='changg@iu.edu',
-
-    # packages=['STProfiler'],
-    packages=find_packages(),
-
-    install_requires=[
-                     'numpy==1.24.4',
-                     'scipy==1.10.1',
-                     'seaborn==0.13.2',
-                     'matplotlib==3.7.5',
-                     'pandas==2.0.3',
-                     'scikit-learn==1.3.2',
-                     'scikit-image==0.21.0',
-                     'imbalanced-learn==0.12.3',
-                     'tqdm==4.66.4',
-                     'segment-anything==1.0',
-                     'pillow==10.3.0'
-                     ],
-)
+if __name__ == "__main__":
+    setup(name="STProfiler")
